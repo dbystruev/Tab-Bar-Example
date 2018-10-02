eb3739e1a27fbea40d3ae423e40ad950f9a1abcc
Denis Bystruev <dbystruev@me.com>
2018-10-02 21:22:24 +0300

    Initial Commit

---
+++ Tab Bar Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tab Bar Example
+//
+//  Created by Denis Bystruev on 02/10/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
